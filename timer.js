e87d5a7333c5340b0f3a3c58e648501b95a27af5
Oleksii Blekot <aleksey.blekot@gmail.com>
2023-10-27 14:57:24 +0300

    CSS, fix timer

--- timer.js
+++ timer.js
@@ -1,4 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
+    let countdown = 10;//90 * 60;
+    let timerInterval;
+    let timerRunning = true; // Flag to track whether the timer is running
+    var playPromise
+    var isAudioPlaying = false
+    let audio
+
     // Function to format time as "HH:MM:SS"
     function formatTime(seconds) {
         const hours = Math.floor(seconds / 3600);
@@ -10,45 +17,107 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Function to play a sound
     function playSound() {
-        const audio = new Audio('timer.mp3'); // Replace with the correct audio file path
-        audio.play();
+        if (isAudioPlaying) return
+
+        stopSound()
+        const audio = document.getElementById("audioPlayer"); // Get the audio element by ID
+        if (audio) {
+            playPromise = audio.play();
+            if (playPromise !== undefined) {
+                playPromise.then(_ => {
+                    // Automatic playback started!
+                    // Show playing UI.
+                    isAudioPlaying = true
+                    console.log("playSound() playPromise.then()");
+                })
+                    .catch(error => {
+                        // Auto-play was prevented
+                        // Show paused UI.
+                        console.log("playSound() playPromise.catch() error:", error);
+                    });
+            }
+        }
+    }
+
+    // Function to stop audio playback
+    function stopSound() {
+        const audio = document.getElementById("audioPlayer"); // Replace with your audio element ID
+        console.log("stopSound(), audio:", audio);
+        if (audio) {
+            console.log("stopSound()");
+            isAudioPlaying = false
+            audio.pause();
+            audio.currentTime = 0;
+        }
+    }
+
+    // Function to update the timer display
+    function updateTimer() {
+        console.log("updateTimer()");
+        const formattedTime = formatTime(countdown);
+        document.getElementById("timer").textContent = formattedTime;
+
+        if (countdown === 0) {
+            clearInterval(timerInterval);
+            document.getElementById("message").textContent = "It's time to take a break and do some exercise!";
+            playSound();
+            document.getElementById("restartButton").style.display = "block"; // Show the "Restart" button
+            document.getElementById("stopPlayButton").style.display = "none"; // Show the "Restart" button
+        } else {
+            countdown--;
+        }
     }
 
     // Function to start the timer
     function startTimer() {
-        // Set the initial countdown time to 90 minutes (90 * 60 seconds)
-        let countdown = 10;// 90 * 60;
-        let timerInterval;
-
-        function updateTimer() {
-            const formattedTime = formatTime(countdown);
-
-            // Display the formatted remaining time
-            document.getElementById("timer").textContent = formattedTime;
-
-            // Check if the countdown has reached zero
-            if (countdown === 0) {
-                clearInterval(timerInterval);
-                document.getElementById("message").textContent = "It's time to take a break and do some exercise!";
-                playSound(); // Play a sound when the timer finishes
-                setTimeout(restartTimer, 5000); // Automatically restart the timer after 5 seconds (adjust as needed)
-            } else {
-                countdown--;
-            }
-        }
-
-        function restartTimer() {
-            countdown = 10;//90 * 60; // Restart the timer
-            document.getElementById("message").textContent = ""; // Clear the message
-            updateTimer(); // Start the timer again
-            timerInterval = setInterval(updateTimer, 1000); // Resume the timer interval
-        }
-
-        // Start the timer when the page loads
+        console.log("startTimer()");
         updateTimer();
         timerInterval = setInterval(updateTimer, 1000);
+        document.getElementById("stopPlayButton").textContent = "Stop"; // Change button text to "Stop"
+        timerRunning = true; // Set the timer as running
     }
 
-    // Call the startTimer function
-    startTimer();
+    // Function to stop or play the timer
+    function stopPlayTimer() {
+        console.log("stopPlayTimer() timerRunning:", timerRunning);
+        if (timerRunning) {
+            clearInterval(timerInterval);
+            document.getElementById("stopPlayButton").textContent = "Play"; // Change button text to "Play"
+            document.getElementById("restartButton").style.display = "block"; // Show the "Restart" button
+        } else {
+            startTimer();
+            document.getElementById("stopPlayButton").textContent = "Stop"; // Change button text to "Stop"
+            document.getElementById("restartButton").style.display = "none"; // Hide the "Restart" button
+        }
+        timerRunning = !timerRunning; // Toggle the timer state
+    }
+
+    // Function to restart the timer
+    function restartTimer() {
+        console.log("restartTimer()");
+        countdown = 10;//90 * 60;
+        document.getElementById("message").textContent = "";
+        updateTimer();
+
+        // Clear the existing timerInterval (if it's running)
+        if (timerInterval) {
+            clearInterval(timerInterval);
+        }
+
+        // Stop any audio playback
+        stopSound();
+
+        // Start a new timer interval
+        timerInterval = setInterval(updateTimer, 1000);
+        document.getElementById("restartButton").style.display = "none"; // Hide the "Restart" button
+        document.getElementById("stopPlayButton").textContent = "Stop"; // Change button text to "Stop"
+        timerRunning = true; // Set the timer as running
+    }
+
+    // Event listeners for buttons
+    document.getElementById("stopPlayButton").addEventListener("click", stopPlayTimer);
+    document.getElementById("restartButton").addEventListener("click", restartTimer);
+
+    // Start the timer when the page loads
+    startTimer(); // Call the startTimer function to initiate the timer
 });
